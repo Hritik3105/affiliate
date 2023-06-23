f141d5ea8d32a8a8fcc578e10e8cb0f825d515ec
webdeveloper510 <webdeveloper510@gmail.com>
2023-06-23 10:21:22 +0530

    stable

--- CampaignApp/views.py
+++ CampaignApp/views.py
@@ -2084,10 +2084,12 @@ class InfluencerCampSale(APIView):
                                 amount_to_paid=cal_amt/sales_done[0]["influencerfee"]
                                 print(amount_to_paid)
                             PaymentDetails.objects.filter(vendor=self.request.user.id,campaign=i["campaign_detail"],influencer=i["influencer"]).update(sales=cal_amt,influencerfee=i["influener_fee"],offer=i["offer"],amount=amount_to_paid)
-                        # else:
-                        #     print("helloqwwwwwwwwwww")
-                        #     print(sales_done)
-                        #     PaymentDetails.objects.filter(vendor=self.request.user.id,campaign=i["campaign_detail"],influencer=i["influencer"]).update(sales=i["sales"],influencerfee=i["influener_fee"],offer=i["offer"],amount=i["amount"])
+                        else:
+                            amount_to_paid=cal_amt/sales_done[0]["influencerfee"]
+                            print(amount_to_paid)
+                            cal_amt=int(sales_done[0]["salespaid"])-int(i["sales"])
+                            print("addd",cal_amt)
+                            PaymentDetails.objects.filter(vendor=self.request.user.id,campaign=i["campaign_detail"],influencer=i["influencer"]).update(sales=cal_amt,influencerfee=i["influener_fee"],offer=i["offer"],amount=amount_to_paid)
                   
                     
                 else:
