ed040976b31ea93222ed3eb1d67bab46f886faf2
webdeveloper510 <webdeveloper510@gmail.com>
2023-06-23 11:22:00 +0530

    stable

--- CampaignApp/views.py
+++ CampaignApp/views.py
@@ -2070,24 +2070,20 @@ class InfluencerCampSale(APIView):
                 if emp_check:
                     for i in data_max:
                         sales_done=PaymentDetails.objects.filter(vendor=self.request.user.id,campaign=i["campaign_detail"],influencer=i["influencer"]).values("salespaid","sales","influencerfee")
-                        print("sales_doneeee",sales_done[0]["sales"])
-                     
-                        if int(sales_done[0]["sales"])!=0:
-                            print("not qual to zero")
-                        #     print("helo saless",sales_done[0]["sales"])
-                        #     if int(sales_done[0]["sales"]) != 0:
-                            if sales_done[0]["salespaid"]!= None:
+                        print("sales",sales_done)
+                        
+                        if sales_done[0]["salespaid"]:
+                            print("helo saless",sales_done[0]["sales"])
+                            if int(sales_done[0]["sales"]) != 0:
                                 cal_amt=int(sales_done[0]["salespaid"])-int(sales_done[0]["sales"])
-                        else:
-                            print("wqual to zero")
-                        #         print("checkffgggg",int(sales_done[0]["salespaid"]))
-                        #         print(int(i["sales"]))
-                            if sales_done[0]["salespaid"]!= None:
+                            else:
+                                print("checkffgggg",int(sales_done[0]["salespaid"]))
+                                print(int(i["sales"]))
                                 cal_amt=int(sales_done[0]["salespaid"])-int(i["sales"])
-                        #         print("addd",cal_amt)
-                        #         amount_to_paid=cal_amt/sales_done[0]["influencerfee"]
-                        #         print(amount_to_paid)
-                        PaymentDetails.objects.filter(vendor=self.request.user.id,campaign=i["campaign_detail"],influencer=i["influencer"]).update(sales=cal_amt,influencerfee=i["influener_fee"],offer=i["offer"],amount=i["amount"])
+                                print("addd",cal_amt)
+                                amount_to_paid=cal_amt/sales_done[0]["influencerfee"]
+                                print(amount_to_paid)
+                            PaymentDetails.objects.filter(vendor=self.request.user.id,campaign=i["campaign_detail"],influencer=i["influencer"]).update(sales=cal_amt,influencerfee=i["influener_fee"],offer=i["offer"],amount=amount_to_paid)
                         # else:
                         #     print("helloqwwwwwwwwwww")
                         #     print(sales_done)
