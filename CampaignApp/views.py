e01b881bf0729560dc320627e87c9d1a2c038be0
webdeveloper510 <webdeveloper510@gmail.com>
2023-06-27 19:43:40 +0530

    changes

--- CampaignApp/views.py
+++ CampaignApp/views.py
@@ -2008,9 +2008,9 @@ class InfluencerCampSale(APIView):
                    
                     for i in influencer_sales_for_campaign[key]:
                         str_detail=StripeDetails.objects.filter(influencer=key,vendor=self.request.user.id).values("account_id")
-                      
+                        check=Campaign.objects.filter(id=i["campaign_id"]).values("influencer_fee","offer","campaign_name")
+
                         if str_detail:
-                            check=Campaign.objects.filter(id=i["campaign_id"]).values("influencer_fee","offer","campaign_name")
                             if  check[0]["offer"] == "percentage":
                                 amount=i["sales"] / check[0]["influencer_fee"] 
                             
@@ -2031,6 +2031,7 @@ class InfluencerCampSale(APIView):
                             
                             lst_data.append(infl_dict)  
                         else:
+                            
                             if  check[0]["offer"] == "percentage":
                                 amount=int(i["sales"])  / check[0]["influencer_fee"] 
                             
