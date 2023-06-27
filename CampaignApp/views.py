378e406756e11f7be335ac8630082b9ad7e1121a
webdeveloper510 <webdeveloper510@gmail.com>
2023-06-27 09:57:45 +0530

    commit

--- CampaignApp/views.py
+++ CampaignApp/views.py
@@ -1978,7 +1978,7 @@ class InfluencerCampSale(APIView):
                 sale=list(sales_by_coupon.keys())
                 amount=list(sales_by_coupon.values())
               
-            
+                print("salesss",sale)
                 campaign_ids =  Campaign.objects.filter(vendorid=self.request.user.id).values_list('id', flat=True) 
 
             
