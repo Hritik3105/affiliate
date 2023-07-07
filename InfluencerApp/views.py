a50aa60e7ae00b4c0ccb1c91a0ce2bad3ebcb3b8
webdeveloper510 <webdeveloper510@gmail.com>
2023-07-07 17:12:39 +0530

    updates

--- InfluencerApp/views.py
+++ InfluencerApp/views.py
@@ -1369,27 +1369,113 @@ class InfluencerApplied(APIView):
     
     def get(self,request):
         camp_id=request.query_params.get('value')
-        print("-------------",camp_id)
+        infl_ids=ModashInfluencer.objects.get(influencerid=self.request.user.id)
         
+        camp_accept=Campaign.objects.filter(id=camp_id).update(campaign_status=1)
+        infl_accept=Notification.objects.filter(campaignid_id=camp_id,influencerid_id=infl_ids.id).update(send_notification=2)
+
+        vendor_id=Campaign.objects.get(id=camp_id)
+        infl_ids=ModashInfluencer.objects.get(influencerid=self.request.user.id)
+        
+        VendorCampaign.objects.create(campaign_status=1,camapaignid_id=camp_id,influencerid_id=infl_ids.id,vendor=vendor_id.id)
+        camp=Product_information.objects.get(campaignid=camp_id)
+       
+        
+        
+        # dict1={
+        #         "campaignid_id":camp_id,
+        #         "campaign_name":camp.campaignid.campaign_name ,
+        #         "product":[{
+        #         "product_name":camp.product_name,
+        #         "coupon_name":ast.literal_eval(camp.coupon_name),
+        #         "amount":ast.literal_eval(camp.amount),
+        #         "discount_type":ast.literal_eval(camp.discount_type),
+        #         "product_id": camp.product_id,
+        #     }]
+        # }    
         
         lst=[]
         final_lst=[]
-   
-        camp=Product_information.objects.get(campaignid=camp_id)
-        print(camp)
+        res=[]
+        res2=[]    
         
-        dict1={
-                "campaignid_id":camp_id,
-                "campaign_name":camp.campaignid.campaign_name ,
-                "product":[{
-                "product_name":camp.product_name,
-                "coupon_name":camp.coupon_name,
-                "amount":camp.amount,
-                "discount_type":camp.discount_type,
-                "product_id": camp.product_id,
-            }]
-        }    
-        return Response({"data":dict1},status=status.HTTP_200_OK)   
+        vendor_codes=VendorCampaign.objects.filter(influencerid_id=infl_ids.id,campaign_status=1)
+        print(vendor_codes)
+        if vendor_codes.exists():
+            res.append(vendor_codes)
+        else:
+            res=""
+        
+        if res:
+            
+            for i in res:
+                z=(i.values("campaignid"))
+                res2.append(z)
+        
+  
+            for i in res2:
+                ids = [q['campaignid'] for q in i]
+                for id in ids:
+                    lst.append(id)
+                    
+        set_data=set(lst)
+     
+        fin_value=set_data
+        for i in fin_value:
+         
+            camp=Product_information.objects.filter(campaignid_id=i).values()
+            campaign_obj59=Product_information.objects.filter(campaignid_id=i).select_related("campaignid")
+        
+            for k in campaign_obj59:
+               pass
+
+        
+            for i in range(len(camp)):
+                cop=(camp[i]["coupon_name"])
+                amt=(camp[i]["amount"])
+             
+                if cop:
+                  
+                    couponlst=ast.literal_eval(cop)
+                else:
+                    couponlst=cop
+                    
+                if amt:
+                    
+                    amtlst=ast.literal_eval(amt)
+                else:
+                    amtlst=amt
+                    
+               
+            
+                dict1={
+                    "campaignid_id":camp[i]["campaignid_id"],
+                    "campaign_name": k.campaignid.campaign_name ,
+                    
+                    "status":k.campaignid.campaign_status,
+                    "product":[{
+                    "product_name":camp[i]["product_name"],
+                    "coupon_name":couponlst,
+                    "amount":amtlst,
+                    "product_id": camp[i]["product_id"],
+                    
+                }]
+                }
+                final_lst.append(dict1)
+                    
+    
+        result={}
+        for i, record in enumerate(final_lst):
+         
+            if record["campaignid_id"] in result:
+                result[record["campaignid_id"]]["product"].append(record["product"][0])
+            else:
+               
+                result[record["campaignid_id"]] = record
+                result[record["campaignid_id"]]["product"] = record["product"]
+        val=list(result.values())
+        
+        return Response({"data":val},status=status.HTTP_200_OK)   
      
      
 # class AssignCoupon(APIView):
