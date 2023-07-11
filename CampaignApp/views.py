c76fd43803aa9f24c8223fe2eb45a30b5676b05d
webdeveloper510 <webdeveloper510@gmail.com>
2023-07-11 12:40:39 +0530

    changes

--- CampaignApp/views.py
+++ CampaignApp/views.py
@@ -1932,7 +1932,7 @@ class InfluencerCampSale(APIView):
             print("data_maxx",data_max)
             for i in data_max:
                 print(i)
-                emp_check=PaymentDetails.objects.exclude(vendor=self.request.user.id,campaign_id=i["campaign_detail"])
+                emp_check=PaymentDetails.objects.filter(vendor=self.request.user.id,campaign_id=i["campaign_detail"]).exists()
                 print(emp_check)
             if emp_check:
                 for i in data_max:
