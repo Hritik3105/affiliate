0a6db7232a83a5887cee979d88edd92c9196db3f
webdeveloper510 <webdeveloper510@gmail.com>
2023-07-13 13:09:07 +0530

    updates

--- CampaignApp/utils.py
+++ CampaignApp/utils.py
@@ -107,10 +107,11 @@ def ExpiryCoupondelete(self,request):
             print(str_lst)
             cop_id=influencer_coupon.objects.filter(coupon_name__in=str_lst,vendor=self.request.user.id).values_list("coupon_id",flat=True)
             print(cop_id)
-            url =f'https://{SHOPIFY_API_KEY}:{SHOPIFY_API_SECRET}@{acc_tok[1]}/admin/api/{API_VERSION}/price_rules/{cop_id[0]}.json'
-    
-    
-            response = requests.delete(url,headers=headers)
+            if cop_id:
+                url =f'https://{SHOPIFY_API_KEY}:{SHOPIFY_API_SECRET}@{acc_tok[1]}/admin/api/{API_VERSION}/price_rules/{cop_id[0]}.json'
+        
+        
+                response = requests.delete(url,headers=headers)
 
-            delete_coup=influencer_coupon.objects.filter(coupon_name__in=str_lst).delete()
+                delete_coup=influencer_coupon.objects.filter(coupon_name__in=str_lst).delete()
     return "DONE"
