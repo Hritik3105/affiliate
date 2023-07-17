195e5fd713c2e7c59225e359ea812005491d0da0
webdeveloper510 <webdeveloper510@gmail.com>
2023-07-17 10:34:26 +0530

    updates

--- AdminApp/views.py
+++ AdminApp/views.py
@@ -51,8 +51,8 @@ def show(request):
     
     for shop in vendor_data:
         get_tok = Store.objects.filter(store_name=shop["shopify_url"]).values("access_token", "store_name")
-    values=dashboard_value(request,get_tok,sales_reports1,vendor_store,sale_val,total_order_count,total_sales)
-     
+        values=dashboard_value(request,get_tok,sales_reports1,vendor_store,sale_val,total_order_count,total_sales)
+       
     rouded_value=values[0]
     sales_reports1=values[1]
     total_order_count=values[2]
