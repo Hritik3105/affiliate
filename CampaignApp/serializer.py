eaa8fa28b08584726da4ec18e5129046de575f10
webdeveloper510 <webdeveloper510@gmail.com>
2023-07-19 11:23:08 +0530

    updatesc

--- CampaignApp/serializer.py
+++ CampaignApp/serializer.py
@@ -169,7 +169,7 @@ class CampaignUpdateSerializer(serializers.ModelSerializer):
         if influencer_fee >100 and offer=="percentage":
             raise serializers.ValidationError("Influencer fee must be less than or equal to 100.")
         
-        if influencer_fee < 0 and offer=="percentage":
+        if influencer_fee < 0 and offer=="percentage" or offer == "commission":
             raise serializers.ValidationError("Influencer fee must be in positive.")
         return influencer_fee
 
