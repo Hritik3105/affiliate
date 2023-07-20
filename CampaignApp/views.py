b0ca1c7e767b9f0bdafc208f9ef1cefa4c20e833
webdeveloper510 <webdeveloper510@gmail.com>
2023-07-20 13:22:29 +0530

    updates

--- CampaignApp/views.py
+++ CampaignApp/views.py
@@ -1977,7 +1977,7 @@ class InfluencerCampSale(APIView):
                        
                         tranfer_money=transferdetails.objects.filter(vendor=self.request.user.id,influencer=i["influencer"],campaign=i["campaign_detail"]).values("amountpaid")
                         print("transfer",tranfer_money)
-                        PaymentDetails.objects.filter(vendor=self.request.user.id,campaign_id=i["campaign_detail"]).update(sales=i["sales"],influencerfee=i["influener_fee"],offer=i["offer"],amount=i["amount"],influencer_id=i["influencer"],vendor_id=self.request.user.id,campaign_id=i["campaign_detail"],account_id=i["account"],amount=)
+                        PaymentDetails.objects.filter(vendor=self.request.user.id,campaign_id=i["campaign_detail"]).update(sales=i["sales"],influencerfee=i["influener_fee"],offer=i["offer"],amount=i["amount"],influencer_id=i["influencer"],vendor_id=self.request.user.id,campaign_id=i["campaign_detail"],account_id=i["account"])
 
                         
                 # if emp_check:
