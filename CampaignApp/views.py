904810ef1ec4cb7bd5aa9d5022601f7de2aa275a
webdeveloper510 <webdeveloper510@gmail.com>
2023-07-21 12:18:48 +0530

    remaining

--- CampaignApp/views.py
+++ CampaignApp/views.py
@@ -266,10 +266,10 @@ class RequestCampaign(APIView):
                 req_id=serializer.save(vendorid_id=self.request.user.id,status=1)
                 val_lst=(request.data["product_discount"])
                 
-                product=Product_information()
-                product.vendor_id=self.request.user.id
-                product.campaignid_id=req_id.id
-                product.save()
+                # product=Product_information()
+                # product.vendor_id=self.request.user.id
+                # product.campaignid_id=req_id.id
+                # product.save()
                 return Response({"success":"Campaign create successfully","product_details":serializer.data},status=status.HTTP_200_OK)
 
         else:
@@ -756,7 +756,7 @@ class   MarketplaceList(APIView):
     def get(self,request):
         lst=[]
         final_lst=[]
-   
+        sliced=""
         
         campaign_obj=Campaign.objects.filter(vendorid_id=self.request.user.id,status=1,draft_status=0,campaign_exp=1)
         if campaign_obj:
