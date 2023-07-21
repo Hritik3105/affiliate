0f4db5259d92e3cd2ed229a9e5651d7f20837ac6
webdeveloper510 <webdeveloper510@gmail.com>
2023-07-21 11:37:41 +0530

    changes

--- CampaignApp/views.py
+++ CampaignApp/views.py
@@ -287,23 +287,28 @@ class UpdateCampaign(APIView):
             if val_lst2:
    
                 for i in  range (len(val_lst2)):
+                    if val_lst2[i]["coupon_name"]:
+                      
+                        for j in val_lst2[i]["coupon_name"]:
                         
-                    for j in val_lst2[i]["coupon_name"]:         
-                        match_data=Product_information.objects.filter(coupon_name__contains=j,vendor_id=self.request.user.id).exists()
-                        
-                    
-                        dict1={str(val_lst2[i]["coupon_name"]):match_data}
-                        
-                        cup_lst.append(dict1)
-                        coup_lst.append(match_data)
-                        
+                            
+                            match_data=Product_information.objects.filter(coupon_name__contains=j,vendor_id=self.request.user.id,campaignid_id=pk).exists()
+                            if match_data== False:
+                            
+                                match_data=Product_information.objects.filter(coupon_name__contains=j,vendor_id=self.request.user.id).exists()
 
-                    if True in coup_lst:
-                        cop=(list(dict1.keys())[0])
-                        
-                        cop_lst=ast.literal_eval(cop)
-                        
-                        return Response({"error": cop_lst},status=status.HTTP_410_GONE)
+                                
+                                dict1={str(val_lst2[i]["coupon_name"]):match_data}
+                                cup_lst.append(dict1)
+                                coup_lst.append(match_data)
+                                
+                            
+                                
+
+                                if True in coup_lst:
+                                    cop=(list(dict1.keys())[0])
+                                    cop_lst=ast.literal_eval(cop)
+                                    return Response({"error": cop_lst},status=status.HTTP_410_GONE)
                         
                 req_id=serializer.save()
                 val_lst=(request.data["product_discount"])
@@ -1004,11 +1009,11 @@ class RequestSents(APIView):
                                 cup_lst.append(dict3)
                                 coup_lst.append(match_data)
                 
-                        if True in coup_lst:
-                            cop=(list(dict3.keys())[0])
-                            cop_lst=ast.literal_eval(cop)
-                            
-                            return Response({"error": cop_lst},status=status.HTTP_410_GONE)
+                            if True in coup_lst:
+                                cop=(list(dict3.keys())[0])
+                                cop_lst=ast.literal_eval(cop)
+                                
+                                return Response({"error": cop_lst},status=status.HTTP_410_GONE)
 
                     req_id=serializer.save(status=2,vendorid_id=self.request.user.id)
                     infll=serializer.data["influencer_name"]
