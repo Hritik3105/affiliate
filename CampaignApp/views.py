bed825c80900d2322fa2e59e3f7763cdc68dc976
webdeveloper510 <webdeveloper510@gmail.com>
2023-07-21 11:50:23 +0530

    changes

--- CampaignApp/views.py
+++ CampaignApp/views.py
@@ -217,7 +217,7 @@ class RequestCampaign(APIView):
                     print("-------",val_lst2)
                    
                     for i in  range (len(val_lst2)):
-                        print(type("00000000000000000",val_lst2[i]["coupon_name"]))
+                        print(type(val_lst2[i]["coupon_name"]))
                         if val_lst2[i]["coupon_name"]:
                             for j in val_lst2[i]["coupon_name"]:         
                                 match_data=Product_information.objects.filter(coupon_name__contains=j,vendor_id=self.request.user.id).exists()
