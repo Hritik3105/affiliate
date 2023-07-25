bb98b965862485d4cb6896df3d70d1c8d10e1cbe
webdeveloper510 <webdeveloper510@gmail.com>
2023-07-25 10:34:18 +0530

    updates

--- CampaignApp/views.py
+++ CampaignApp/views.py
@@ -2797,7 +2797,7 @@ class AdminTransfer(APIView):
                 campaign_name = Campaign.objects.filter(id=campaign_id).values_list('campaign_name', flat=True).first() 
                 admin_tra.append({"campaign_id":campaign_id,"sale":round(sale,2), "campaign_name":campaign_name,"commission":commission_val,"admin_part":round(admin_part,2),"account":admin_account,"admin_id":admin_acc})
             
-            empty=PaymentDetails.objects.filter(admin=admin_acc).exists()
+            empty=PaymentDetails.objects.filter(vendor=self.request.user.id,admin=admin_acc).exists()
            
             if empty == True:
                 for i in admin_tra:
