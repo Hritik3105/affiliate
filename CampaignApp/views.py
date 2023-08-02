35761bfc2af4e0a0bb189532bbef5a43490381bd
webdeveloper510 <webdeveloper510@gmail.com>
2023-08-02 12:15:39 +0530

    prefetch

--- CampaignApp/views.py
+++ CampaignApp/views.py
@@ -1018,7 +1018,7 @@ class RequestSents(APIView):
                         for j in val_lst2[i]["coupon_name"]:        
                             
                             match_data=Product_information.objects.filter(coupon_name__contains=j,vendor_id=self.request.user.id).first()
-                            print("=-----------------------------",match_data)
+                            print("=-----------------------------",match_data.coupon_name)
                             match_data222=Product_information.objects.filter(coupon_name__in=j,vendor_id=self.request.user.id).exists()
                             
                             if match_data == True:
