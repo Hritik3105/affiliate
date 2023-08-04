b5a6586fea0d28843ae12622e08af323bf31d139
webdeveloper510 <webdeveloper510@gmail.com>
2023-08-04 14:56:09 +0530

    changes

--- CampaignApp/views.py
+++ CampaignApp/views.py
@@ -2987,7 +2987,7 @@ class AdminTransfer(APIView):
                 if empty == True:
                 
                     for i in admin_tra:
-                        print("rrrr")
+                        
                         emp_check=PaymentDetails.objects.filter(vendor=self.request.user.id,campaign_id=i["campaign_id"]).exists()
                     
                         if emp_check == True:
@@ -3007,7 +3007,8 @@ class AdminTransfer(APIView):
 
 
                         else:
-                            print("dddddddd")
+                            print(empty)
+                            print(i)
                             if admin_acc==" ":
                             
                                 PaymentDetails.objects.create(sales=i["sale"],influencerfee=i["commission"],offer=i["offer"],amount=admin_acc,admin_id=admin_acc,vendor_id=self.request.user.id,campaign_id=i["campaign_id"],account_id=i["account"])
