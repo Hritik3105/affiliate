2c8276f8fe1c340df982314d4874fa75111808d5
webdeveloper510 <webdeveloper510@gmail.com>
2023-08-04 15:30:16 +0530

    changes

--- CampaignApp/views.py
+++ CampaignApp/views.py
@@ -2011,12 +2011,12 @@ class InfluencerCampSale(APIView):
             sale=list(sales_by_coupon.keys())
             amount=list(sales_by_coupon.values())
     
-            print("ssssssssss",sales_by_coupon)
+        
             campaign_ids =  Campaign.objects.filter(vendorid=self.request.user.id).values_list('id', flat=True) 
           
             influencer_sales_for_campaign = {}
             for coupon_name, sales in sales_by_coupon.items():
-               
+                print("dsfdsfdsf",coupon_name)
                 influencer_ids = influencer_coupon.objects.filter(coupon_name=coupon_name,vendor=self.request.user.id).values("influencer_id", "coupon_name")
             
                 for influencer in influencer_ids:
