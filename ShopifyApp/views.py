5a5a12f41441bc73869a8634aebac95fec6847f4
webdeveloper510 <webdeveloper510@gmail.com>
2023-08-07 10:25:38 +0530

    commit

--- ShopifyApp/views.py
+++ ShopifyApp/views.py
@@ -660,6 +660,10 @@ class ProductEditCodeView(APIView):
             
             
         discount = request.data.get('discount_code')
+        if discount:
+                if len(discount)<3:
+                    return Response({'error': 'discount code must be three or more than three character long'}, status=status.HTTP_400_BAD_REQUEST)
+
         
         if discount == None:
             discount=old_title
