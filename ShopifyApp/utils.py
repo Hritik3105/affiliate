9d52cd84560114b55e4b4979ea3267abd14a8965
webdeveloper510 <webdeveloper510@gmail.com>
2023-08-08 13:24:01 +0530

    changes

--- ShopifyApp/utils.py
+++ ShopifyApp/utils.py
@@ -3,6 +3,7 @@ from StoreApp.models import Store
 import requests
 from Affilate_Marketing.settings import base_url ,headers ,SHOPIFY_API_KEY,SHOPIFY_API_SECRET,API_VERSION
 from rest_framework.response import Response
+from rest_framework import status
 
 
 def access_token(self,request):
