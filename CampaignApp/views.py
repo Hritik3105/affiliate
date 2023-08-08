f08d03b794520f1d1901656070c8884c53b64ac2
webdeveloper510 <webdeveloper510@gmail.com>
2023-08-08 17:08:20 +0530

    changes

--- CampaignApp/views.py
+++ CampaignApp/views.py
@@ -999,8 +999,8 @@ class ProductList(APIView):
             headers= {"X-Shopify-Access-Token": i.access_token}
             url=f"https://{i.store_name}/admin/api/{API_VERSION}/products.json?status=active"
             response = requests.get(url, headers=headers)
-            print(response)
-            for data in response.json():
+            print(response.json)
+            for data in response.json:
                 print(data)
                 # id=data["products"][0]["id"]
                 # title=data["products"][0]["title"]
