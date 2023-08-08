02f51e2804bd090dae73bade3b67ac4d541877b3
webdeveloper510 <webdeveloper510@gmail.com>
2023-08-08 18:58:18 +0530

    updates

--- CampaignApp/views.py
+++ CampaignApp/views.py
@@ -3378,7 +3378,7 @@ class CommisssionFilter(APIView):
             print("00000000000",match_data)
         if commission:
             match_data=Product_information.objects.filter(Q(campaignid__campaign_status=0)|Q(campaignid__campaign_status=1),campaignid__offer = commission)
-            print("000000044440000",match_data)
+            print("00000004444dssd0000",match_data)
         else:
             match_data=Product_information.objects.filter(Q(campaignid__campaign_status=0)|Q(campaignid__campaign_status=1),product_id=product)
             print("000000000444565700",match_data)
