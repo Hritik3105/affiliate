033db5b5fb50ec96eafef5ae33687e4cca8dd928
webdeveloper510 <webdeveloper510@gmail.com>
2023-08-10 11:37:53 +0530

    edit cop

--- ShopifyApp/views.py
+++ ShopifyApp/views.py
@@ -584,7 +584,7 @@ class ProductEditCodeView(APIView):
                             # upd_val=ast.literal_eval(k.coupon_name)[edit_index]=val
                             # print("ddd",ast.literal_eval(k.coupon_name))
                             
-                            Product_information.object.filter(coupon_id=k.coupon_id).update(coupon_name=ast.literal_eval(k.coupon_name))
+                            Product_information.object.filter(coupon_id=k.coupon_id).update(coupon_name=(k.coupon_name))
                 influencer_coupon.objects.filter(id=infludb_id).update(influencer_id_id=influencer_id,amount=float(amount),coupon_name=discount,vendor_id=self.request.user.id)
             
                 return Response({'message': 'Discount Edit successfully','title': discount,"discount_type":discount_type,'amount':amt,"id":price_rule,"influencer":influencer_id})
