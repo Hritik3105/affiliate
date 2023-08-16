5e45a11070b962b3b28a2ff17d93ab066e90cd73
webdeveloper510 <webdeveloper510@gmail.com>
2023-08-16 11:17:24 +0530

    changes

--- CampaignApp/views.py
+++ CampaignApp/views.py
@@ -2101,7 +2101,9 @@ class InfluencerCampSale(APIView):
                             else:
                                 coup_dict[campaign_id] = list_value
                     print(coup_dict)
-                   
+                    for campaign_id, coupon_names in coup_dict.items():
+                        if coupon_name in coupon_names:
+                            print(coupon_name)
                     pro_dataqq=Product_information.objects.filter(coupon_name__contains=coupon_name,campaignid__in=modash_data,vendor=self.request.user.id)
                     
                     
