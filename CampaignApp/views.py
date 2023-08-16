524e68e4d58294c3c57e0718b13cd744028319e6
webdeveloper510 <webdeveloper510@gmail.com>
2023-08-16 11:08:54 +0530

    changes

--- CampaignApp/views.py
+++ CampaignApp/views.py
@@ -2089,7 +2089,7 @@ class InfluencerCampSale(APIView):
                     influencer_id = influencer["influencer_id"]
                 
                     modash_data = Campaign.objects.filter(influencer_name__contains=influencer_id, id__in=campaign_ids,vendorid=self.request.user.id).values_list("id",flat=True)
-                    check=Product_information.objects.filter(coupon_name__contains=i,vendor_id=self.request.user.id).values("coupon_name","campaignid")
+                    check=Product_information.objects.filter(coupon_name__contains=coupon_name,campaignid__in=modash_data,vendor=self.request.user.id).values("coupon_name","campaignid")
                     for z in check:
                         if "coupon_name" in z:
                             list_value = eval(z["coupon_name"])
